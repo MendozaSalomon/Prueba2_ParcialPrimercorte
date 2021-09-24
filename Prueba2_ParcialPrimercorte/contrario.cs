83dfba30ee6aa442991e00877efc5647e62dc647
valeriaqte <90458281+valeriaqte@users.noreply.github.com>
2021-09-23 21:10:15 -0500

    EJERCICIO 4 3ER INT

---
+++ Prueba2_ParcialPrimercorte/contrario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba2_ParcialPrimercorte
+{
+    class contrario
+    {
+
+        public string Invertir(string cadena)
+        {
+            // Convertir a arreglo
+            char[] cadenaComoCaracteres = cadena.ToCharArray();
+            // Invertir el arreglo usando métodos incorporados
+            Array.Reverse(cadenaComoCaracteres);
+            // Convertir de nuevo el arreglo a cadena
+            return new string(cadenaComoCaracteres);
+
+
+        }
+
+
+
+    }
+}
